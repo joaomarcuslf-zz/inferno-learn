76a351cac7794de79a40e27faf99484c5384c29f
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2017-05-27 11:24:24 -0300

    Refactor to use functional component

--- todo-app/src/view/todo.js
+++ todo-app/src/view/todo.js
@@ -1,5 +1,4 @@
 import Inferno from "inferno";
-import Component from "inferno-component";
 import { observer } from "inferno-mobx";
 import {
   Hero,
@@ -11,90 +10,76 @@ import {
   Link
 } from "../components";
 
-@observer
-export default class TodoView extends Component {
-  render() {
-    const model = this.props.model;
-
-    return (
-      <Hero className="is-primary is-fullheight">
-        <HeroHead>
-          <header class="nav">
-            <div class="container">
-              <div class="nav-center">
-                <h1 class="title is-1">Todo List</h1>
-              </div>
-            </div>
-          </header>
-        </HeroHead>
-
-        <HeroBody>
-
-          <Container className="has-text-centered">
-            <Columns className="is-mobile">
-              <Column>
-                <Link onClick={() => model.add()}>New Todo</Link>
-              </Column>
-              <Column>
-                <Link onClick={() => model.load()}>Reload Todos</Link>
-              </Column>
-              <Column>
-                <Link onClick={() => model.save()}>Save Todos</Link>
-              </Column>
-            </Columns>
-
-            <Container>
-              {model.todos.map((todo, i) => (
-                <SingleTodoView key={todo.id} model={model} todo={todo} />
-              ))}
-            </Container>
-          </Container>
-        </HeroBody>
-      </Hero>
-    );
-  }
-}
-
-@observer
-export class SingleTodoView extends Component {
-  render() {
-    const model = this.props.model;
-    const todo = this.props.todo;
-
-    return (
-      <Container className="has-text-centered">
-        #{todo.id}
-        <strong>{todo.text}</strong>
-        <i>{todo.done ? "DONE!" : ""}</i>
-
-        <br />
-
-        <div class="field has-addons has-addons-centered">
-          <div class="control">
-            <label class="checkbox is-medium">
-              <input
-                type="checkbox"
-                checked={todo.done}
-                onChange={e => (todo.done = e.target.checked)}
-              />
-            </label>
+const TodoView = observer(({ model }) => (
+  <Hero className="is-primary is-fullheight">
+    <HeroHead>
+      <header class="nav">
+        <div class="container">
+          <div class="nav-center">
+            <h1 class="title is-1">Todo List</h1>
           </div>
-
-          <div class="control">
-            <input
-              class="input"
-              type="text"
-              value={todo.text}
-              onChange={e => (todo.text = e.target.value)}
-              placeholder="Text input"
-            />
-          </div>
-
-          <button class="button is-danger" onClick={() => model.remove(todo)}>
-            X
-          </button>
         </div>
+      </header>
+    </HeroHead>
+
+    <HeroBody>
+      <Container className="has-text-centered">
+        <Columns className="is-mobile">
+          <Column>
+            <Link onClick={() => model.add()}>New Todo</Link>
+          </Column>
+          <Column>
+            <Link onClick={() => model.load()}>Reload Todos</Link>
+          </Column>
+          <Column>
+            <Link onClick={() => model.save()}>Save Todos</Link>
+          </Column>
+        </Columns>
+
+        <Container>
+          {model.todos.map((todo, i) => (
+            <SingleTodoView key={todo.id} model={model} todo={todo} />
+          ))}
+        </Container>
       </Container>
-    );
-  }
-}
+    </HeroBody>
+  </Hero>
+));
+
+export const SingleTodoView = observer(({ model, todo }) => (
+  <Container className="has-text-centered">
+    #{todo.id}
+    <strong>{todo.text}</strong>
+    <i>{todo.done ? "DONE!" : ""}</i>
+
+    <br />
+
+    <div class="field has-addons has-addons-centered">
+      <div class="control">
+        <label class="checkbox">
+          <input
+            type="checkbox"
+            checked={todo.done}
+            onChange={e => (todo.done = e.target.checked)}
+          />
+        </label>
+      </div>
+
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          value={todo.text}
+          onChange={e => (todo.text = e.target.value)}
+          placeholder="Text input"
+        />
+      </div>
+
+      <button class="button is-danger" onClick={() => model.remove(todo)}>
+        X
+      </button>
+    </div>
+  </Container>
+));
+
+export default TodoView;
